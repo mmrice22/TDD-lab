280e8192f415869c046d8fd43315701ab5270063
Melissa Rice <mmrice22@gmail.com>
2020-02-12 09:00:44 -0500

    almost fin

--- src/changehandler.js
+++ src/changehandler.js
@@ -14,18 +14,40 @@ class ChangeHandler {
    * The parameter "type" is a string that is either quarter, dime, nickel, or penny
    */
   insertCoin(typeOfCoin) {
-
+    if (typeOfCoin === "quarter") {
+      this.cashTendered += 25;
+    } else if (typeOfCoin === "dime") {
+      this.cashTendered += 10;
+    } else if (typeOfCoin === "nickel") {
+      this.cashTendered += 5;
+    } else if (typeOfCoin === "penny") {
+      this.cashTendered += 1;
+    }
   }
 
   /**
    * Returns true if enough coins have been inserted to at least meet the amountDue
    */
   isPaymentSufficient() {
-      
+    if (this.cashTendered > this.amountDue) {
+      return true;
+    } else if (this.cashTendered < this.amountDue) {
+      return false;
+    } else if ((this.cashTendered = this.amountDue)) {
+      return true;
+    }
   }
 
   giveChange() {
     // TODO return the correct change in the following format...
+    while (this.cashTendered > this.amountDue) {
+      if (this.cashTendered - "quarter") {
+        quarter += 1;
+        this.cashTendered = 25;
+      } else if (this.cashTendered !== "quarter") {
+        quarter = 0;
+      }
+    }
     return {
       quarters: 0,
       dimes: 0,
--- tests/changehandler.test.js
+++ tests/changehandler.test.js
@@ -1,11 +1,72 @@
 let { ChangeHandler } = require("../src/changehandler");
 
 describe("Tests for ChangeHandler", function() {
-
-    // Set up a test below...
-    test("The ChangeHandler class is defined.", function() {
-        // Remember, you can arrange, act, and assert...start small
-        expect(ChangeHandler).toBeDefined();
-    });
-
+  // Set up a test below...
+  test("The ChangeHandler class is defined.", function() {
+    let test = new ChangeHandler(1);
+    // Remember, you can arrange, act, and assert...start small
+    expect(test).toBeDefined();
+  });
+  test("The amountDue is set based on an argument.", function() {
+    let test = new ChangeHandler(1);
+    expect(test.amountDue).toBe(1);
+  });
+  test("cashTendered is set to zero", function() {
+    let test = new ChangeHandler(999);
+    expect(test.cashTendered).toBe(0);
+  });
+  test("Inserting a quarter adds 25", function() {
+    let test = new ChangeHandler(999);
+    test.insertCoin("quarter");
+    expect(test.cashTendered).toBe(25);
+  });
+  test("Inserting a dime adds 10", function() {
+    let test = new ChangeHandler(999);
+    test.insertCoin("dime");
+    expect(test.cashTendered).toBe(10);
+  });
+  test("Inserting a nickel adds 5", function() {
+    let test = new ChangeHandler(999);
+    test.insertCoin("nickel");
+    expect(test.cashTendered).toBe(5);
+  });
+  test("Inserting a penny adds 1", function() {
+    let test = new ChangeHandler(999);
+    test.insertCoin("penny");
+    expect(test.cashTendered).toBe(1);
+  });
+  test("continue to add on to the amount", function() {
+    let test = new ChangeHandler(999);
+    test.insertCoin("penny");
+    test.insertCoin("dime");
+    expect(test.cashTendered).toBe(11);
+  });
+  test("Returns true if cashTendered more than amountDue. ", function() {
+    let test = new ChangeHandler(25);
+    amountDue = 25;
+    test.insertCoin("quarter");
+    test.insertCoin("quarter");
+    expect(test.isPaymentSufficient()).toBe(true);
+  });
+  test("Returns false if cashTendered less than amountDue. ", function() {
+    let test = new ChangeHandler(75);
+    amountDue = 75;
+    test.insertCoin("quarter");
+    test.insertCoin("quarter");
+    expect(test.isPaymentSufficient()).toBe(false);
+  });
+  test("Returns true if cashTendered equal to amountDue. ", function() {
+    let test = new ChangeHandler(25);
+    amountDue = 25;
+    test.insertCoin("quarter");
+    expect(test.isPaymentSufficient()).toBe(true);
+  });
+  test("give change of 1 quarter, 1 nickel, 2 penny", function() {
+    let test = new ChangeHandler(0);
+    test.insertCoin("quarter");
+    test.insertCoin("nickel");
+    test.insertCoin("penny");
+    test.insertCoin("penny");
+    expect(test.giveChange()).toEqual();
+  });
 });
